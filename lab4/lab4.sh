0f0a64ab5ddc5f7be4eef9f36af15c0aa222f91a
Andryushin Maxim <unlimimage@yandex.ru>
2019-11-27 00:57:47 +0300

    lab4 done

---
+++ lab4/lab4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ps -eo euid,ruid,comm | tail -n +2 >temp.out #tail чтобы не учиьвыать строку "EUID RUID COMMAN"
+exec 0<temp.out
+while read euid ruid comm 
+do
+	if [[ $euid != $ruid ]]; then
+		echo "process $comm's euid differs from ruid"
+	fi
+done
