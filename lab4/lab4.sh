556a00abed8ec40b9414388ff80a30e3da3914c3
Andryushin Maxim <unlimimage@yandex.ru>
2019-11-27 01:05:03 +0300

    add comment

--- lab4/lab4.sh
+++ lab4/lab4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ps -eo euid,ruid,comm | tail -n +2 >temp.out #tail чтобы не учиьвыать строку "EUID RUID COMMAN"
+ps -eo euid,ruid,comm | tail -n +2 >temp.out #tail чтобы не учитывать строку "EUID RUID COMMAND"
 exec 0<temp.out
 while read euid ruid comm 
 do
